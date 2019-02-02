ff54104b3498875716f33fcb17492e634fd1cfae
Lewis Mendoza <Lewis_mendoza@ucne.edu.do>
2019-02-02 17:37:12 -0400

    Creado los Proyectos y las entidades

---
+++ Entities/Depositos.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class Depositos
+    {
+        public int DepositoId { get; set; }
+        public DateTime Fecha { get; set; }
+        public int CuentaId { get; set; }
+        public string Concepto { get; set; }
+        public decimal Monto { get; set; }
+
+        public Depositos()
+        {
+
+        }
+    }
+}
