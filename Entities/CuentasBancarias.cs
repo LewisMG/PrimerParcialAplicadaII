e5ad1d71eb8c6889644a1aa7b578e732f040280c
Lewis Mendoza <Lewis_mendoza@ucne.edu.do>
2019-02-02 18:00:17 -0400

    Instalado entityframework, creado repositorioBase y IRepository,

--- Entities/CuentasBancarias.cs
+++ Entities/CuentasBancarias.cs
@@ -1,20 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Entities
 {
+    [Serializable]
     public class CuentasBancarias
     {
-        public int CuentaId { get; set; }
+        [Key]
+        public int CuentaBancariaId { get; set; }
+
         public DateTime Fecha { get; set; }
+
         public string Nombre { get; set; }
-        public decimal Balance { get; set; }
+
+        public int Balance { get; set; }
+
+        public virtual List<Depositos> Detalle { get; set; }
+
 
         public CuentasBancarias()
         {
+            this.Detalle = new List<Depositos>();
+        }
+
+        public void AgregarDetalle(int DepositoId, DateTime Fecha, int CuentaId, string Concepto, int Monto)
+        {
+            this.Detalle.Add(new Depositos(DepositoId, Fecha, CuentaId, Concepto, Monto));
         }
     }
 }
--- Entities/Depositos.cs
+++ Entities/Depositos.cs
@@ -1,22 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Entities
 {
+    [Serializable]
     public class Depositos
     {
+        [Key]
         public int DepositoId { get; set; }
-        public DateTime Fecha { get; set; }
+        public DateTime Fecha { get; set; }                   
         public int CuentaId { get; set; }
         public string Concepto { get; set; }
         public decimal Monto { get; set; }
 
+        [ForeignKey("CunetaId")]
+        public virtual CuentasBancarias CuentaBancaria { get; set; }
+
         public Depositos()
         {
+            DepositoId = 0;
+            Fecha = DateTime.Now;
+            CuentaId = 0;
+            Concepto = string.Empty;
+            Monto = 0;
+        }
 
+        public Depositos(int depositoId, DateTime fecha, int cuentaId, string concepto, decimal monto)
+        {
+            DepositoId = depositoId;
+            Fecha = fecha;
+            CuentaId = cuentaId;
+            Concepto = concepto;
+            Monto = monto;
         }
     }
 }
